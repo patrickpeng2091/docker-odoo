e1eaa620841f767643d77f64da7d2a5f1de027eb
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-27 22:38:11 -0400

    Create odoo.sh

---
+++ odoo.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+### In odoo.sh (make sure this file is chmod +x):
+# `/sbin/setuser openerp` runs the given command as the user `openerp`.
+# If you omit that part, the command will be run as root.
+
+exec start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid odoo --make-pidfile --exec /usr/bin/openerp-server -- --config=/etc/odoo/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log >>/var/log/odoo.log 2>&1
