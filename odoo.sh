9cf4761092817e306814e7e78c7177c86e210b55
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-06 23:35:10 -0400

    Update odoo.sh

--- odoo.sh
+++ odoo.sh
@@ -4,3 +4,4 @@
 # If you omit that part, the command will be run as root.
 
 exec start-stop-daemon --start --quiet --pidfile /var/run/openerp-server.pid --chuid odoo --make-pidfile --exec /usr/bin/openerp-server -- --config=/etc/odoo/openerp-server.conf --logfile=/var/log/openerp/openerp-server.log >>/var/log/odoo.log 2>&1
+
